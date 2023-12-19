dc3387d0aaa0aec6fb5b9a28324c34f9af9894e8
mis401 <62945435+mis401@users.noreply.github.com>
2023-12-19 23:57:33 +0100

    prettier

--- EventDriven/event-driven-backend/src/auth/auth.service.ts
+++ EventDriven/event-driven-backend/src/auth/auth.service.ts
@@ -11,75 +11,76 @@ import { Request } from 'express';
 export class AuthService {
     constructor(private prisma: PrismaService, private jwt: JwtService) {}
 
-   async signup(dto: AuthDto) {
-    const {email, password} = dto;
+    async signup(dto: AuthDto) {
+        const {email, password} = dto;
 
-    const foundUser = await this.prisma.user.findUnique({where:{email}})
+        const foundUser = await this.prisma.user.findUnique({where:{email}})
 
-    if(foundUser){
-        throw new BadRequestException('Email already exists')
-    }
-    const hashedPassword = await this.hashPassword(password)
-
-    await this.prisma.user.create({
-        data: {
-            email,
-            hashedPassword
+        if(foundUser){
+            throw new BadRequestException('Email already exists')
         }
-    })
+        const hashedPassword = await this.hashPassword(password)
+
+        await this.prisma.user.create({
+            data: {
+                email,
+                hashedPassword
+            }
+        })
 
     return {message: 'signup was successful'};
-   }
-
-   async signin(dto: AuthDto, req: Request, res: Response){
-    const {email,password} = dto;
-
-    const foundUser = await this.prisma.user.findUnique({where:{email}})
-    if(!foundUser){
-        throw new BadRequestException('Wrong credentials')
     }
 
-    const isMatch = await this.comparePasswords(
-        {
-            password, 
-            hash: foundUser.hashedPassword
-        });
-    if(!isMatch){
-        throw new BadRequestException('Wrong credentials')
+    async signin(dto: AuthDto, req: Request, res: Response){
+        const {email,password} = dto;
+
+        const foundUser = await this.prisma.user.findUnique({where:{email}})
+        if(!foundUser){
+            throw new BadRequestException('Wrong credentials')
+        }
+
+        const isMatch = await this.comparePasswords(
+            {
+                password, 
+                hash: foundUser.hashedPassword
+            });
+        if(!isMatch){
+            throw new BadRequestException('Wrong credentials')
+        }
+
+        //sign jwt and return to the user
+        const token = await this.signToken(
+            {
+                id: foundUser.id, 
+                email: foundUser.email
+            });
+
+        if( !token){
+            throw new ForbiddenException()
+        }
+
+        res.cookie('token', token);
+        
+        return res.send({message: 'Logged in successfully'});
     }
 
-    //sign jwt and return to the user
-    const token = await this.signToken(
-        {
-            id: foundUser.id, 
-            email: foundUser.email
-        });
-
-    if( !token){
-        throw new ForbiddenException()
+    async singout(req: Request, res:Response) {
+        res.clearCookie('token');
+        return res.send({message: 'Logged out successfully'})
     }
 
-    res.cookie('token', token);
-    
-    return res.send({message: 'Logged in successfully'});
-   }
+    async hashPassword(password: string){
+        const saltOrRounds = 10;
+        return await bcrypt.hash(password, saltOrRounds)
+    }
 
-   async singout(req: Request, res:Response) {
-    res.clearCookie('token');
-    return res.send({message: 'Logged out successfully'})
-   }
+    async comparePasswords(args: {password:string , hash:string}){
+        return await bcrypt.compare(args.password, args.hash);
+    }
 
-   async hashPassword(password: string){
-    const saltOrRounds = 10;
-    return await bcrypt.hash(password, saltOrRounds)
-   }
+    async signToken(args: {id:number, email:string}){
+        const payload = args
 
-   async comparePasswords(args: {password:string , hash:string}){
-    return await bcrypt.compare(args.password, args.hash);
-   }
-   async signToken(args: {id:number, email:string}){
-    const payload = args
-
-    return this.jwt.signAsync(payload, {secret: JwtSecret})
-   }
+        return this.jwt.signAsync(payload, {secret: JwtSecret})
+    }
 }
