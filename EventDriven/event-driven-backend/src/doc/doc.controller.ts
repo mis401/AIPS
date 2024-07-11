626ebd5b340a23a35914d0e4183faea2766ad001
Andjelija <mijajlovic.andjelija11@gmail.com>
2024-07-11 03:27:04 +0200

    C:/Program Files/Git/

--- EventDriven/event-driven-backend/src/doc/doc.controller.ts
+++ EventDriven/event-driven-backend/src/doc/doc.controller.ts
@@ -1,13 +1,22 @@
 import { BadRequestException, Body, Controller, Get, HttpException, HttpStatus, Post, Query, Req, ValidationPipe } from '@nestjs/common';
 import { DocService } from './doc.service';
 import { NewDocumentDTO } from 'src/dtos/new-document.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('doc')
 export class DocController {
     constructor(private DocService: DocService) {
-        
     }
+    private readonly userService: UserService
+    @Post('open')
+  async openDocument(@Body() body: { userId: number, communityId: number, documentName: string }) {
+    return this.userService.updateUserStatus(body.userId, 'working', body.documentName);
+  }
 
+  @Post('close')
+  async closeDocument(@Body() body: { userId: number, communityId: number }) {
+    return this.userService.updateUserStatus(body.userId, 'idle');
+  }
     @Get('get')
     async getDoc(@Query('id') id: number) {
         const idNum = Number(id);
