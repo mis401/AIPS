b17923075f4c29b16b7c15ca9c4d1020a268dc8f
Andjelija <mijajlovic.andjelija11@gmail.com>
2024-07-15 17:34:49 +0200

    C:/Program Files/Git/

--- EventDriven/event-driven-backend/src/message/message.module.ts
+++ EventDriven/event-driven-backend/src/message/message.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
-<<<<<<< HEAD
 import { MessageController } from './message.controller';
-=======
-// import { MessageController } from './message.controller';
->>>>>>> cf58162d736fb332e8911708408072ffe4153091
 import { ChatModule } from 'src/chat-mq/chat.module';
 
 @Module({
   imports: [ChatModule],
-<<<<<<< HEAD
  controllers: [MessageController],
-=======
-  // controllers: [MessageController],
->>>>>>> cf58162d736fb332e8911708408072ffe4153091
   providers: [MessageService],
 })
 export class MessageModule {}
